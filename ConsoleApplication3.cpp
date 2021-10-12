dd8bc949ce9d955ca940a30d6a3dba8822d5da0d
Vitalii Fedyk <vitalii.fedyk.it.2021@lpnu.ua>
2021-10-12 17:55:30 +0300

    4.7

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double xp, xk, x, dx, eps, a, R, S;
+	int n;
+
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(10) << "x" << " |"
+		<< setw(10) << "arcth(x)" << " |"
+		<< setw(10) << "S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+	 
+		x = xp;
+		while (x <= xk && abs(x)>1)
+		{
+			n = 0;
+			a = 1.0 / x;
+			S = a;
+			do {
+				R = (2 * n + 1) / ((2 * n + 3) * x * x);
+				n++;
+				a *= R;
+				S += a;
+			} while (abs(a) >= eps);
+
+			cout << "|" << setw(10) << setprecision(2) << x << " |"
+				<< setw(10) << setprecision(5) << atanh(1.0 / x) << " |"
+				<< setw(10) << setprecision(5) << S << " |"
+				<< setw(5) << n << " |"
+				<< endl;
+		   x += dx;
+	}
+	cout << "-------------------------------------------------" << endl;
+	
+	return 0;
+}
+
